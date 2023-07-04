94bfcdb8236fe0f4e06763000b48931e63d81645
Qkprahlad101 <k.prahlad101@gmail.com>
2023-07-05 03:22:13 +0530

    Loading images using Coil library.

--- app/src/main/java/com/example/jetpackprofilecard/MainActivity.kt
+++ app/src/main/java/com/example/jetpackprofilecard/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.example.jetpackprofilecard
 
-import android.annotation.SuppressLint
-import android.graphics.drawable.ShapeDrawable
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -35,12 +31,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.jetpackprofilecard.ui.theme.MyTheme
 import com.example.jetpackprofilecard.ui.theme.lightGreen
-import com.example.jetpackprofilecard.ui.theme.veryLightGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -117,11 +115,13 @@ fun ProfilePicture(drawableId: Int, onlineStatus: Boolean) {
         border = BorderStroke(2.dp, if(onlineStatus) lightGreen else Color.Red),
         modifier = Modifier.padding(16.dp)
     ) {
-        Image(
-            painter = painterResource(id = drawableId),
-            contentDescription = " ",
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(drawableId)
+                .crossfade(true)
+                .build(),
             modifier = Modifier.size(72.dp),
-            contentScale = ContentScale.Crop
+            contentDescription = null,
         )
     }
 }
